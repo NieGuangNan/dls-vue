b1745c9d075dee8a62232bc62c9e235544338071
GuanTing <guanting@lean-intelligence.com>
2019-03-14 16:19:54 +0800

    vue-demo2

---
+++ src/router/index.js
@@ -0,0 +1,67 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import {sync} from "vuex-router-sync";
+import store from "store";
+import types from "store/mutation-types";
+import auth from "common/auth";
+import dashboard from "pages/dashboard.vue";
+import NotFoundView from "components/404.vue";
+import menuList from "pages/sys/menu.vue";
+import role from "pages/sys/role.vue";
+import resource from "pages/sys/resource.vue";
+import login from "pages/login.vue";
+import app from "../App.vue";
+import sysUser from "pages/sys/user.vue";
+import userAdd from "pages/sys/userAdd.vue";
+import resetPwd from "pages/resetPwd.vue";
+import test1 from "pages/test1/testPage.vue"
+
+//加载路由中间件
+Vue.use(VueRouter)
+
+//定义路由
+const router = new VueRouter({
+  routes: [
+    {path: '/login', component: login},
+    {
+      path: '/test', component: app, children: [
+        {path: '/test/1/1/1', component: test1},
+        {path: '*', component: NotFoundView}
+      ]
+    },
+    {
+      path: '', component: app, children: [
+        {path: '/resetPwd', component: resetPwd},
+        {path: '/index', component: dashboard},
+        {path: '/sys/menuList', component: menuList},
+        {path: '/sys/roleList', component: role},
+        {path: '/sys/userList', component: sysUser},
+        {path: '/sys/userAdd', component: userAdd},
+        {path: '/sys/resource', component: resource}
+      ]
+    },
+    {path: '*', component: NotFoundView}
+  ],
+  mode: 'history'
+})
+
+sync(store, router)
+
+const {state} = store
+
+router.beforeEach((route, redirect, next) => {
+  if (state.device.isMobile && state.sidebar.opened) {
+    store.commit(types.TOGGLE_SIDEBAR, false)
+  }
+  if (!auth.loggedIn() && route.path !== '/login') {
+    next({
+      path: '/login',
+      query: {redirect: route.fullPath}
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
