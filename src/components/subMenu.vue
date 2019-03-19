cedcea28cfbdb046f89454c0b8cbd53704f1987b
GuanTing <guanting@lean-intelligence.com>
2019-03-19 18:24:24 +0800

    vue-demo2

--- src/components/subMenu.vue
+++ src/components/subMenu.vue
@@ -1,13 +1,21 @@
 <template>
-  <el-submenu :index="item.href" v-if="item.children && item.children.length>0" class="el-menu-sub">
+
+  <el-submenu :index="item.href" v-if="item.children && item.children.length >0 " class="el-menu-sub">
     <template slot="title"><i :class="item.icon"></i><span>{{$t(item.name)}}</span></template>
     <template v-for="child in item.children">
       <sub-menu v-if="child.children && child.children.length>0" :param="child"></sub-menu>
-      <el-menu-item :index="child.href" v-else><i :class="child.icon"></i><span>{{$t(child.name)}}</span></el-menu-item>
+      <el-menu-item :index="child.href" v-else>
+        <i :class="child.icon"></i><span>{{$t(child.name)}}</span>
+      </el-menu-item>
     </template>
   </el-submenu>
-  <el-menu-item :index="item.href" v-else class="el-menu-each"><i :class="item.icon"></i><span>{{$t(item.name)}}</span></el-menu-item>
-</template>
+    <el-menu-item  :index="item.href" v-else class="el-menu-each">
+          <i :class="item.icon"></i><span>{{$t(item.name)}}</span>
+    </el-menu-item>
+  </template>
+
+
+
 <script>
   import subMenu from "./subMenu.vue"
 
@@ -19,7 +27,7 @@
     },
     components: {
       subMenu
-    }
+    },
   }
 </script>
 <style>
--- src/pages/charts/mixedChart.vue
+++ src/pages/charts/mixedChart.vue
@@ -1,54 +1,91 @@
 <template>
   <div id="wrap">
-    <e-template :id="id" :option="option" :style="style"></e-template>
+    <el-button type="default" @click="a()">测试</el-button>
+    <echarts :option="option" :height='bodyHeight/4'></echarts>
+    <echarts :option="option1" :height='bodyHeight/4'></echarts>
   </div>
 
 </template>
 
 <script>
-  import eTemplate from '@/components/echartTemplate'
-  const option={
-    title: { text: '在Vue中使用echarts' },
+  import echarts from '@/components/echarts'
+
+  const option = {
+    title: {text: '在Vue中使用echarts'},
     tooltip: {},
     xAxis: {
-      data: ["数据1","数据2","数据3","数据4","数据5","数据6"]
+      data: ["数据1", "数据2", "数据3", "数据4", "数据5", "数据6"]
     },
     yAxis: {},
-    legend:{
-      show:true
+    legend: {
+      show: true
     },
     series: [{
       name: '销量',
-      color:'pink',
+      color: 'pink',
       type: 'bar',
       data: [5, 20, 36, 10, 10, 20]
-    },{
+    }, {
       name: '好评数',
       type: 'line',
-      color:'orange',
+      color: 'orange',
       data: [8, 20, 39, 14, 20, 25]
     }]
-  }
+  };
+  const option1 = {
+    title: {text: '在Vue中使用echarts'},
+    tooltip: {},
+    xAxis: {
+      data: ["数据1", "数据2", "数据3", "数据4", "数据5", "数据6"]
+    },
+    yAxis: {},
+    legend: {
+      show: true
+    },
+    series: [{
+      name: '销量666',
+      color: 'pink',
+      type: 'bar',
+      data: [5, 2, 36, 10, 10, 20]
+    }, {
+      name: '好评数',
+      type: 'line',
+      color: 'orange',
+      data: [8, 2, 39, 14, 20, 25]
+    }]
+  };
+
   export default {
     name: "mixedChart",
-    components:{eTemplate},
-    data(){
-      return{
-        id:"myChart",
-        option:option,
-        style:{width: '100%', height: '600px'}
+    components: {echarts},
+    props: {
+      bodyHeight: {type: Number}
+    },
+    data() {
+      return {
+        id: "myChart",
+        option: option,
+        option1: option1,
+        style: {width: '100%', height: '600px'}
       }
     },
+    methods: {
+      a() {
+        console.log(211111);
 
+          this.option = option1;
+        console.log(this.option)
+      }
+    }
 
   }
 </script>
 
 <style scoped>
- #wrap{
-   background: #fff;
-   padding: 10px;
- }
+  #wrap {
+    background: #fff;
+    padding: 10px;
+  }
 
 
 </style>
